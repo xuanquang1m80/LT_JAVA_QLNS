b2a9b3750f9842b7d2f07a9a77e8842a8087605e
cienp <cienp@DESKTOP-5UB0M8A>
2024-06-24 22:32:41 +0700

    CRUD Complete Bằng Cấp , and create file editModal.js new

--- qlns/src/main/resources/static/js/editModal.js
+++ qlns/src/main/resources/static/js/editModal.js
@@ -1,3 +1,4 @@
+//Bằng Cấp
 document.addEventListener("DOMContentLoaded", function() {
     var editButtons = document.querySelectorAll(".editButton");
     editButtons.forEach(function(button) {
@@ -12,3 +13,20 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+// Chức vụ
+ document.addEventListener("DOMContentLoaded", function() {
+        var editButtons = document.querySelectorAll(".editButton");
+        editButtons.forEach(function(button) {
+            button.addEventListener("click", function() {
+                var id = button.getAttribute("data-id");
+                var tenChucVu = button.getAttribute("data-tenChucVu");
+                var luong = button.getAttribute("data-luong");
+                var moTa = button.getAttribute("data-moTa");
+
+                document.getElementById("editId").value = id;
+                document.getElementById("inputTenChucVuEdit").value = tenChucVu;
+                document.getElementById("inputLuongEdit").value = luong;
+                document.getElementById("inputMoTaEdit").value = moTa;
+            });
+        });
+    });
