23f1f544ee25ea9a30bfcf3bc3c0cd54d1cd3d09
cienp <cienp@DESKTOP-5UB0M8A>
2024-06-25 00:18:40 +0700

    CRUD Complete Chuyên Môn

---
+++ qlns/src/main/java/com/ltjava/qlns/service/ChuyenMonService.java
@@ -0,0 +1,27 @@
+package com.ltjava.qlns.service;
+
+import com.ltjava.qlns.model.ChuyenMon;
+import com.ltjava.qlns.repository.ChuyenMonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChuyenMonService {
+
+    @Autowired
+    private ChuyenMonRepository chuyenMonRepository;
+
+    public List<ChuyenMon> findAll() {
+        return chuyenMonRepository.findAll();
+    }
+
+    public void save(ChuyenMon chuyenMon) {
+        chuyenMonRepository.save(chuyenMon);
+    }
+
+    public void deleteById(Long id) {
+        chuyenMonRepository.deleteById(id);
+    }
+}
