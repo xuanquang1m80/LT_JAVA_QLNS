b43a9672f7f2d54fcb10fa253069f4652a2c8c15
minhle.28062003@gmail.com <minhle.28062003@gmail.com>
2024-06-25 00:31:59 +0700

    No font enddd

--- qlns/src/main/java/com/ltjava/qlns/controller/PhongBanController.java
+++ qlns/src/main/java/com/ltjava/qlns/controller/PhongBanController.java
@@ -5,10 +5,11 @@ import com.ltjava.qlns.service.PhongBanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+// @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/phongbans")
 public class PhongBanController {
@@ -49,3 +50,4 @@ public class PhongBanController {
         return ResponseEntity.noContent().build();
     }
 }
+
