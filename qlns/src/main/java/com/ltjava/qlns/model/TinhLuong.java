5d4398c81ce613bd721dd8f404839904bdb4b01b
cienp <cienp@DESKTOP-5UB0M8A>
2024-06-29 19:09:32 +0700

    chuwc nwng tinh uong

--- qlns/src/main/java/com/ltjava/qlns/model/TinhLuong.java
+++ qlns/src/main/java/com/ltjava/qlns/model/TinhLuong.java
@@ -1,7 +1,10 @@
 package com.ltjava.qlns.model;
 
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -27,8 +30,4 @@ public class TinhLuong {
     Date ngayTinhLuong;
 
     String moTa;
-
-    @ManyToOne
-    @JoinColumn(name = "nhan_vien_id")
-    private NhanVien nhanVien;
 }
--- qlns/src/main/java/com/ltjava/qlns/service/NhanVienService.java
+++ qlns/src/main/java/com/ltjava/qlns/service/NhanVienService.java
@@ -1,8 +1,24 @@
 package com.ltjava.qlns.service;
 
-
+import com.ltjava.qlns.model.NhanVien;
+import com.ltjava.qlns.repository.NhanVienRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NhanVienService {
+
+    @Autowired
+    private NhanVienRepository nhanVienRepository;
+
+    public List<NhanVien> getAllNhanViens() {
+        return nhanVienRepository.findAll();
+    }
+
+    public Optional<NhanVien> getNhanVienById(Long id) {
+        return nhanVienRepository.findById(id);
+    }
 }
